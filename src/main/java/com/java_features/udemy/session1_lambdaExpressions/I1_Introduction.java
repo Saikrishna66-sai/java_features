36738b686769607f9ed8dcf7883ea553cad49230
SaiKrishna66-sai <saikrishnasunny1111@gmail.com>
2023-08-01 14:26:09 +0530

    init comit

---
+++ src/main/java/com/java_features/udemy/session1_lambdaExpressions/I1_Introduction.java
@@ -0,0 +1,12 @@
+package com.java_features.udemy.session1_lambdaExpressions;
+
+public class I1_Introduction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello-world");
+	}
+	
+}
