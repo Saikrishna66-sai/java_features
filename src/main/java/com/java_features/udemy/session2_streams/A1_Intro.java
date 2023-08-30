b509a08ec46269a6c3192407d4141bb19b29de4e
SaiKrishna66-sai <saikrishnasunny1111@gmail.com>
2023-08-30 12:35:39 +0530

    added intro

--- src/main/java/com/java_features/udemy/session2_streams/A1_Intro.java
+++ src/main/java/com/java_features/udemy/session2_streams/A1_Intro.java
@@ -1,5 +1,20 @@
 package com.java_features.udemy.session2_streams;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class A1_Intro {
 
+	public void m1() {
+		ExecutorService s = Executors.newFixedThreadPool(5);
+		//for(int i = 0;i>10;i++) {
+			Runnable runnableTask = () ->{
+				
+			};
+		//}
+		
+		
+		s.execute(runnableTask);
+		s.shutdown();
+	}
 }
